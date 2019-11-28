a9b3e7b79ccb956132af486f4fa9e5c57c7a3648
Geronejr55 <56701309+Geronejr55@users.noreply.github.com>
2019-11-28 16:56:56 -0500

    Update project3.s
    It goes and jumps to label then jumps to print function

--- project3.s
+++ project3.s
@@ -4,9 +4,16 @@
 	output: .asciiz "\n"
 	invalid: .asciiz "NaN"
 	comma: .asciiz ","
+	
 .text
 main :
 	li $v0,8	
 	la $a0,data 
 	li $a1, 1001	
 	syscall
+	
+	jal SubprogramA
+continue1:
+	j print
+
+
